2b90766a3e8e01fc517455d7c12427f530c36a58
Eduardo Pineda <edibozu@gmail.com>
2020-10-08 19:18:30 -0400

    fixing typos

--- config/connection.js
+++ config/connection.js
@@ -2,8 +2,8 @@ const mysql = require("mysql");
 
 let connection;
 
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.send.JAWSDB_URL);
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
     host: "localhost",
