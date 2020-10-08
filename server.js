99780469d997e3fd9bdf2a69d74571cd14109c5a
Eduardo Pineda <edibozu@gmail.com>
2020-10-08 17:32:11 -0400

    added middleware to server.js file

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
 app.get("/api/config", (req, res) => {
   res.json({
     success: true,
