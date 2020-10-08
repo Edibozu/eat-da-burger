2c15975c0e019f85b8c957b8fb4a1d376071cbf7
Eduardo Pineda <edibozu@gmail.com>
2020-10-08 18:41:16 -0400

    created basics for mysql connection

---
+++ config/connection.js
@@ -0,0 +1,19 @@
+const mysql = require("mysql");
+
+let connection;
+
+if (process.env.JAWDB_URL) {
+  connection = mysql.createConnection(process.send.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    port: "3306",
+    password: "Edge@062190",
+    database: "burger_db",
+  });
+}
+
+connection.connect();
+
+module.exports = connection;
